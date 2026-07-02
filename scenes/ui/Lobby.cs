2f6707fad4f6fa9952b151225b57eb45faa36c8f
kdoricardo10-lang <kdoricardo10@users.noreply.github.com>
2026-07-02 16:00:52 -0300

    p1.png como tela inicial, transicao para world.tscn

--- scenes/ui/Lobby.cs
+++ scenes/ui/Lobby.cs
@@ -4,16 +4,28 @@ namespace StreetChaos
 {
     public partial class Lobby : Control
     {
+        private bool _splashDone;
+
         public override void _Ready()
         {
             var bg = new TextureRect
             {
-                Texture = GD.Load<Texture2D>("res://tela inicial.png"),
+                Texture = GD.Load<Texture2D>("res://p1.png"),
                 ExpandMode = TextureRect.ExpandModeEnum.FitHeightProportional,
                 AnchorsPreset = (int)LayoutPreset.FullRect,
                 StretchMode = TextureRect.StretchModeEnum.KeepAspectCovered
             };
             AddChild(bg);
+        }
+
+        public override void _Input(InputEvent @event)
+        {
+            if (_splashDone) return;
+            if (@event is InputEventKey { Pressed: true })
+            {
+                _splashDone = true;
+                GetTree().ChangeSceneToFile("res://scenes/world/world.tscn");
+            }
         }
     }
 }
